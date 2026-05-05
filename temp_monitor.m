949aa268396b116a4cc001d1e09f6d64283e17c3
rainforoses28 <ssyrd3@nottingham.edu.cn>
2026-05-05 15:43:43 +0800

    undate1

--- temp_monitor.m
+++ temp_monitor.m
@@ -8,3 +8,31 @@ figure;
 Green_LED='D3';
 Yellow_LED='D4';
 Red_LED='D2';
+while true
+    Time_test=toc;
+    A0_voltage = readVoltage(a, 'A0');
+    Temperature_current=(A0_voltage-V_0deg)/T_c;
+    if Time_test>20
+          xlim([Time_test-20,Time_test+5]);
+    end
+    drawnow;
+    Time_test=Time_test+1;
+    if Temperature_record>=18 & Temperature_record<=24
+          writeDigitalPin(a,Green_LED,1)
+          pause_time=1
+    elseif Temperature_record<18
+             writeDigitalPin(a,Yellow_LED,1)
+             pause(0.5)
+             writeDigitalPin(a,Yellow_LED,0)
+             pause(0.5)
+             pause_time=0
+    else
+          writeDigitalPin(a,Red_LED,1)
+          pause(0.25)
+          writeDigitalPin(a,Red_LED,0)
+          pause(0.25)
+          pause_time=0.5
+    end
+    pause(pause_time)
+end
+end
